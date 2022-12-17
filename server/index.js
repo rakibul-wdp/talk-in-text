fb077db48de5b25799bc6a806e124f3bfebfc518
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-17 14:13:58 +0600

    load dummy data another route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { chats } = require('./data/data');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running');
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
 });
 
 app.listen(5000, console.log(`Server started on port 5000`));
