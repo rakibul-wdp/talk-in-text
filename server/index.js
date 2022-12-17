45701db89ed4cafc5f3da6fcc6b597e66a9a4b15
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-17 14:20:25 +0600

    get chat data by dynamic id

--- server/index.js
+++ server/index.js
@@ -11,4 +11,10 @@ app.get('/api/chat', (req, res) => {
   res.send(chats);
 });
 
+app.get('/api/chat/:id', (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+})
+
 app.listen(5000, console.log(`Server started on port 5000`));
