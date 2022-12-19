26612645e0353e7e6aa9d81174c5d9559eb8c5d0
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-19 21:43:10 +0600

    create headline and setup tabs

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,10 +1,40 @@
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-      Home page
-    </div>
+    <Container maxW={"xl"} centerContent>
+      <Box
+        display={"flex"}
+        justifyContent="center"
+        p="3"
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"lg"}
+        borderWidth="1px"
+      >
+        <Text fontSize={"4xl"} fontFamily="Work sans" color={"black"}>
+          Talk-in-Text
+        </Text>
+      </Box>
+      <Box bg={"white"} w="100%" p={4} borderRadius="lg" borderWidth={"1px"}>
+        <Tabs variant="soft-rounded">
+          <TabList mb={'1em'}>
+            <Tab width={'50%'}>Login</Tab>
+            <Tab width={'50%'}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   );
 };
 
