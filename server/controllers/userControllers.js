fe656ae32e0fcd37777dfa5925555a31caf06cfc
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-20 14:18:14 +0600

    create generate token function with jwt

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const generateToken = require('../config/generateToken');
 const User = require('../models/userModel');
 
 const registerUser = asyncHandler(async (req, res) => {
