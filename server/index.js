14311d6f84ce1e1027b180950bf0c20b7243412a
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-20 15:42:33 +0600

    create default error handler

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
 connectDB();
@@ -17,6 +18,14 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server started on port ${PORT}`.yellow.bold));
+app.listen(
+  5000,
+  console.log(
+    `Server started on port ${PORT}`.yellow.bold
+  )
+);
