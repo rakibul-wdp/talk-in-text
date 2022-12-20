b8006218df901d208106e375b2e5928252be3238
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-20 14:47:04 +0600

    hash password before save it

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
@@ -12,6 +13,15 @@ const userSchema = mongoose.Schema({
 },
 { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.modified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
