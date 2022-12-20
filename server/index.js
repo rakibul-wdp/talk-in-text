561c5b3f4b4ff38e49172679406028cd88dbde45
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-20 13:49:49 +0600

    create some mock user route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,15 +13,7 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/chat', (req, res) => {
-  res.send(chats);
-});
-
-app.get('/api/chat/:id', (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000
 
