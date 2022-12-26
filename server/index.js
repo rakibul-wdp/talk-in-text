83db983362d24aab3b491ca6229aa9ad54baa0c5
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-26 17:02:15 +0600

    change dirname

--- server/index.js
+++ server/index.js
@@ -21,13 +21,13 @@ app.use("/api/message", messageRoutes);
 
 // -----------deployment------------
 
-const __dirname = path.resolve();
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
