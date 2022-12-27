7e790ac47fb03a2e3bbd96ec00645e946d3e0dcb
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-27 17:15:12 +0600

    change it again with deploy

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "/client/build/index.html",
+    origin: "https://inspiring-baklava-0afe5d.netlify.app/",
     // credentials: true,
   },
 });
